3d1795407d72400624d04ce4c9d466f6cfa7d198
LinkonJT <linkontripura@gmail.com>
2025-08-12 19:44:05 +0600

    final touch

--- src/components/Shared/AppNavbar.jsx
+++ src/components/Shared/AppNavbar.jsx
@@ -9,6 +9,8 @@ const AppNavbar = () => {
         {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">LJT</span>
       </NavbarBrand>
+   
+        <NavbarToggle />
      
       <NavbarCollapse>
         <NavbarLink href="#home" active>
