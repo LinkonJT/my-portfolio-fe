5bc4d92ba991933bbad0a29077c282d3322216eb
LinkonJT <linkontripura@gmail.com>
2025-08-12 19:16:32 +0600

    smooth scroll added

--- src/components/Shared/AppNavbar.jsx
+++ src/components/Shared/AppNavbar.jsx
@@ -4,7 +4,7 @@ import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle }
 
 const AppNavbar = () => {
     return (
-           <Navbar fluid rounded>
+           <Navbar fluid className="sticky top-0 z-50 bg-white dark:bg-gray-900 shadow">
       <NavbarBrand href="https://flowbite-react.com">
         {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">LJT</span>
